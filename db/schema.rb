f44d9ff60da01826f7ad1b2c4684dbd0c2731c60
Diego Lafuente <dlaf88@gmail.com>
2015-03-24 02:42:58 +0000

    added boostrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324005450) do
+ActiveRecord::Schema.define(version: 20150324015302) do
 
   create_table "items", force: true do |t|
     t.string   "content"
     t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "completed_at"
   end
 
   add_index "items", ["list_id"], name: "index_items_on_list_id"
