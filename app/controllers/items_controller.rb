ada3b2771a9d65457860ee27eb300d820153ae35
Diego Lafuente <dlaf88@gmail.com>
2015-03-24 01:51:44 +0000

    added list and items

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,30 @@
+class ItemsController < ApplicationController
+  before_action :set_list
+  
+  def create 
+    
+    @item = @list.items.create(item_params)
+    redirect_to @list
+  end
+  
+  def destroy
+    @item = @list.items.find(params[:id])
+    if @item.destroy
+      flash[:success] = "Todo Item was deleted"
+      redirect_to @list
+    else 
+      flash[:error] = "Todo list item could not be deleted"
+    end 
+  end 
+  
+  
+  private
+  def set_list
+    @list = List.find(params[:list_id])
+  end 
+  
+  def item_params
+    params[:item].permit(:content)
+  end 
+    
+end
