85dcf311a3f38d7c79ccaabe206f30c0275b8c16
Diego Lafuente <dlaf88@gmail.com>
2015-03-28 15:40:20 +0000

    fixed days_left

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,7 +30,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.html { redirect_to @list, notice: 'List was successfully created.Now add a new item' }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
